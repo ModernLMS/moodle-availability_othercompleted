51d70abe35e19c991529ce132032e6790420a100
Sam Suresh <sam@mu.my>
2019-08-13 09:03:22 +0800

    Update version.php

--- version.php
+++ version.php
@@ -27,4 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2019081300;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 2019081300;
-$plugin->component = 'availability_othercompleted';
+$plugin->component = 'availability_othercompleted';                                                                          
+$plugin->requires = '20171113';                                                                       
+
